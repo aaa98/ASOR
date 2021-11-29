031ac3cc9d4d44e3fca11014e0babe031c0b606b
aaa98 <61359537+aaa98@users.noreply.github.com>
2021-11-29 10:33:21 +0100

    Add files via upload

---
+++ TuberiaSinNombre/Ej1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#define PIPE_W 1
+#define PIPE_R 0
+
+int main(int argv, char** argc){
+
+  if (argv < 2) {
+    printf("Error\n");
+  }
+int fd[2];
+  int pipe (fd);
+
+switch(fork()){
+ case -1:
+      perror("ERROR al hacerl el fork");
+      return 1;
+      break;
+    case 0:
+    //Hijo
+      dup2(fd[PIPE_R],0);
+      close(fd[PIPE_W]);
+      close(fd[PIPE_R]);
+      execlp(argc[3],argc[3],argc[4]);
+    break;
+    default:
+    //Padre
+      dup2(fd[PIPE_W],1);
+      close(fd[PIPE_W]);
+      close(fd[PIPE_R]);
+      execlp(argc[1],argc[1],argc[2]);
+    break;
+  }
+
+}
